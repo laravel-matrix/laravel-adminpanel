f1d19aed6f7f5035e47700377f0be08f97f9623e
Fermin <sysadmin@open-matrix.es>
2018-08-18 05:58:08 +0200

    cambio de fermin

--- app/Http/Controllers/Frontend/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Frontend/Auth/ChangePasswordController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Frontend\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Frontend\User\ChangePasswordRequest;
+
+//
+
 use App\Repositories\Frontend\Access\User\UserRepository;
 
 /**
